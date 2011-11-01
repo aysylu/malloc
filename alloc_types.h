8c9699d5dafee1105067eb845056a28fc7bdcf1c
Mark Mascaro <daigohji@mit.edu>
2011-11-01 13:43:42 -0400

    Some initial routines

--- alloc_types.h
+++ alloc_types.h
@@ -22,29 +22,27 @@ struct arena_bhunk_hdr_s;
 
 // All blocks must have a specified minimum alignment.
 #define ALIGNMENT 8
-
-// Rounds up to the nearest multiple of ALIGNMENT.
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
-
-
-/********************
- * Pages and Chunks *
- ********************/
-
 // Page size; smallest amount allocated to a run of a size class
 #define PAGE_SIZE (4 * 1024) // 4 kB
+// Anything larger than this size must be given at least one page
+#define MAX_SMALL_SIZE (3840)
 // Initial chunk size; heap space given to a new arena when
 // there is only one arena
 #define INITIAL_CHUNK_SIZE (32 * 4 * 1024) // 128 kB
 // Maximum chunk size; maximum size given to an arena
 #define FINAL_CHUNK_SIZE (4 * 1024 * 1024) // 4MB
 
+// Rounds up to the nearest multiple of ALIGNMENT.
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
+#define ALIGNPAGE(size) (((size) + (PAGE_SIZE-1)) & ~(PAGE_SIZE-1))
+#define ALIGNCHUNK(size) (((size) + (FINAL_CHUNK_SIZE-1)) & ~(FINAL_CHUNK_SIZE-1))
+
 // size_t pointers to starts of runs of blocks should be named properly.
 typedef size_t block;
 
-/**********************
- * Small Size Classes *
- **********************/
+/****************
+ * Size Classes *
+ ****************/
 
 // Number of subpage-size size classes
 #define NUM_SMALL_CLASSES 28
@@ -57,14 +55,23 @@ const uint16_t SMALL_CLASS_CONTAINERS[NUM_SMALL_CLASSES] =
    768, 1024, 1280, 1536, 1792, 
    2048, 2304, 2560, 2816, 
    3072, 3328, 3584, 3840}; 
-// We want to do a vaguely-binary search, but these aren't linearly spaced
-// There is probably a bit trick to be done here.
 
-// Get size class of an allocation request
-size_t get_size_class(size_t real_size) {
-  
+// Get small size class of an allocation request
+// Large and HUGE have different handling that
+// comes in before this, so checking for those sizes here
+// is not handy.
+size_t get_small_size_class(size_t real_size) {
+  assert(real_size <= MAX_SMALL_SIZE)
+  // TODO: Replace with binary search, or something... good. 
+  int i;
+  // In descending size order, the moment you don't fit,
+  // return the last thing you fit in.
+  for(i=NUM_SMALL_CLASSES-1; i>=0; i--) {
+    if (real_size > SMALL_CLASS_CONTAINERS[i]) 
+      return SMALL_CLASS_CONTAINERS[i+1];
+  }
+  return SMALL_CLASS_CONTAINERS[0];
 }
-
 
 /****************
  * Arena Chunks *
