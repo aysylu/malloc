d10e9d75f99fbfe4213c08ed8ba4dc3ee459feb4
Aysylu <aysylu7@gmail.com>
2011-11-11 21:30:08 -0500

    incomplete implementation of using free_list for huge allocations; at the moment doesn't update the free_list properly

--- alloc_types.cpp
+++ alloc_types.cpp
@@ -135,18 +135,24 @@ void* arena_hdr::malloc(size_t size) {
       PRINT_TRACE(" The free list has some space; try to pull something from the free list\n");
       // Try to find space to place this allocation
       // Traverse the free list to find if there's a contiguous block of chunks
-      size_t num_contiguous_chunks = 0;
+      size_t num_contiguous_chunks = 1;
 
-      size_t * curr = *(size_t**)free_list;
       size_t * prev = (size_t*)free_list;
+      PRINT_TRACE("head at %p\n", prev);
+      size_t * curr = *(size_t**)free_list;
+      PRINT_TRACE("next element at %p\n", curr);
+
+      // The pointer to the beginnning of contiguous free chunks
+      size_t * beg_cont_free_chunks = (size_t*)free_list;
       PRINT_TRACE(" Traversing the list to find contiguous free chunks\n");
 
       // Iterate over the free_list until
       // we either run off the end of the list OR
       // we find the necessary amount of contiguous free chunks
       // to place our huge allocation in
-      while ((curr != NULL) && (num_contiguous_chunks == num_chunks)) {
-        if (curr - prev == FINAL_CHUNK_SIZE) {
+      while ((curr != NULL) && (num_contiguous_chunks < num_chunks)) {
+        if (curr - prev == INITIAL_CHUNK_SIZE) {
+        PRINT_TRACE("We found two contiguous chunks at addresses %p and %p\n", prev, curr);
           // curr and prev pointer are spaced exactly one chunk size apart
           // so they are contiguous
           num_contiguous_chunks += 1;
@@ -154,6 +160,7 @@ void* arena_hdr::malloc(size_t size) {
           // We either didn't find any contiguous free chunks yet OR
           // the number of contiguous chunks wasn't big enough
           num_contiguous_chunks = 0; //reset the accumulator
+          beg_cont_free_chunks = curr;        // move the beginning of contiguous chunks
         }
         
 	prev = curr;
@@ -162,12 +169,57 @@ void* arena_hdr::malloc(size_t size) {
 
       if (num_contiguous_chunks == num_chunks) {
         // We found the perfect spot in the free_list to place our huge allocation in
-        PRINT_TRACE("We found the perfect spot in the free list to place our huge allocation in\n");
-        new_address = prev - num_chunks;
-         
+        new_address = beg_cont_free_chunks;
+        PRINT_TRACE("We found the perfect spot in the free list to place our huge allocation in at %p\n", new_address);
+        
+        // Now we need to update the free list
+        // Start again from the head of the free_list
+        // and iterate until you find the address
+        PRINT_TRACE("Now cleaning up the free list\n");
+        size_t * succ = *(size_t**)free_list;
+        size_t * pred = (size_t*)free_list;
+
+        // If the new address is the head of the free_list,
+        // prev pointer becomes the head of the free_list
+        if (pred == new_address) {
+        PRINT_TRACE("The head of the list=%p, new_address=%p\n", pred, new_address);
+        PRINT_TRACE("At the same location? %d\n", pred==new_address);
+          pred = prev;
+        PRINT_TRACE("OK, so pred=%p, prev=%p, *pred=%p, *prev=%p\n", pred, prev, *pred, *prev);
+          if (*((size_t*)pred) == NULL) {
+            PRINT_TRACE("The free list should be empty\n");
+            free_list = NULL;
+          }
+        } else {
+          while ((succ != NULL) && (succ != new_address)) {
+	    pred = succ;
+	    succ = (size_t *) *succ;
+          }
+          if (succ == new_address) {
+            *(size_t **)(pred) = (size_t *)prev;
+          }
+        } 
       } else {
         // There were no contiguous free chunks that would fit our huge allocation
         // TODO: Handle the same we would handle it if the free_list was empty
+      // Uh-oh. The free list couldn't help us. This needs a *new chunk*.
+      // Arena is going to demand new space on the heap! Single thread, everything fine.
+      PRINT_TRACE(" Creating a new chunk for this allocation.\n");
+      // A point of care: It may be the case that we have an ungrown arena chunk
+      // on top right now. We need to align the new chunk on top of that.
+      if ((mem_heapsize() - ARENA_HDR_SIZE) % FINAL_CHUNK_SIZE) {
+        // Allocate heap up to the next chunk boundary. If we got here, this is a small run.
+        grow_max((arena_chunk_hdr*)deepest);
+      }
+      void* new_heap = mem_sbrk(num_chunks * FINAL_CHUNK_SIZE);
+      PRINT_TRACE(" Increased the heap by %lu\n", num_chunks * FINAL_CHUNK_SIZE);
+      assert(new_heap != NULL);
+      // Write a new huge_run_hdr into the new space.
+      *(huge_run_hdr*)new_heap = huge_run_hdr(size, num_chunks);
+      // Take note of the deepst object assigned
+      deepest = (size_t*)new_heap;
+      // OK, header in place - let's give them back the pointer, skipping the header
+      new_address = ((byte*) new_heap + HUGE_RUN_HDR_SIZE);
       }
     } else {
       // Uh-oh. The free list couldn't help us. This needs a *new chunk*.
