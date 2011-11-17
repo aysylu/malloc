83226496fc34e6e293c71c3af2ddd4e11780f95a
Aysylu <aysylu7@gmail.com>
2011-11-16 21:58:45 -0500

    Resolved merge conflicts in validator.h

--- validator.h
+++ validator.h
@@ -77,8 +77,8 @@ static int add_range(Type *impl, range_t **ranges, char *lo,
   }
 
   /* The payload must lie within the extent of the heap */
-  if (lo <= mem_heap_lo()) {
-    int ret = sprintf(msg, "Payload must lie within the extent of the heap; the low payload address is %p, and the heap's lo address is %lu\n", lo, mem_heap_lo());
+  if (lo < mem_heap_lo() || hi > mem_heap_hi()) {
+    int ret = sprintf(msg, "Payload must lie within the extent of the heap\n   the payload addresses are %p and %p, and the heapsize is %lu\n  heap lies between %p and %p\n", lo, hi, mem_heapsize(), mem_heap_lo(), mem_heap_hi());
     printf("%s", msg);
     return 0;
   }
